157fabdc24548ae3aad685c22ac9469aef972c91
Thilaksan8 <thilakshanlat@gmail.com>
2025-03-24 23:04:19 +0530

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,9 +1,7 @@
 a=int(input("Enter a :"))
 b=int(input("Enter b :"))
 c=int(input("Enter c :"))
-
-delta=b**2-4*a*c
-
+delta=b**2-4*a*c # calculate delta 
 if delta >=0:
     r1=(-b-delta**(1/2))/(2*a)
     r2=(-b+delta**(1/2))/(2*a)
